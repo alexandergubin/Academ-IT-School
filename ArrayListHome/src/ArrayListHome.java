f7a85a8149c59b88b913a9df44e623564e0653bd
alexandergubin <gubin.alexander88@gmail.com>
2018-06-15 23:11:34 +0600

    добавлена задача ArrayListHome

---
+++ ArrayListHome/src/ArrayListHome.java
@@ -0,0 +1,59 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayListHome {
+    public static void main(String[] args) {
+        String filePath = ".\\ArrayListHome\\src\\input.txt";
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        ArrayList<Integer> arrayList2 = new ArrayList<>(Arrays.asList(1, 2, 1, 5, 6, 5, 3, 2, 6, 1, 5, 3, 2, 6, 5, 1, 3, 2, 6, 5));
+
+        System.out.println("ArrayList из файла");
+        System.out.println(readFileToArrayList(filePath).toString());
+
+        System.out.println("Удалить все четные");
+        System.out.println(deleteEvenNumbers(arrayList).toString());
+
+        System.out.println("Удалить все повторы");
+        System.out.println(deleteRepeats(arrayList2).toString());
+    }
+
+    private static ArrayList<String> readFileToArrayList(String filePath) {
+        ArrayList<String> result = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new FileInputStream(filePath), "UTF-8")) {
+            while (scanner.hasNext()) {
+                result.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static ArrayList<Integer> deleteEvenNumbers(ArrayList<Integer> arrayList) {
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            if (arrayList.get(i) % 2 == 0) {
+                arrayList.remove(i);
+            }
+        }
+        return arrayList;
+    }
+
+    private static ArrayList<Integer> deleteRepeats(ArrayList<Integer> arrayList) {
+        ArrayList<Integer> result = new ArrayList<>(arrayList);
+        int i = 0;
+        while (i < result.size()) {
+            int tmp = result.get(i);
+            for (int j = result.size() - 1; j > i; j--) {
+                if (result.get(j) == tmp) {
+                    result.remove(j);
+                }
+            }
+            i++;
+        }
+        return result;
+    }
+}
